aac700a5fc5f6b71779ee0ac27b5e06a46ab1c6d
Gleb Pankov <g@gpnkv.com>
2024-09-27 20:52:43 +0200

    conda environment setup script

---
+++ conda_script.sh
@@ -0,0 +1,11 @@
+conda create --name fincrime -c conda-forge \
+  python=3.11 \
+  geopandas==0.14.2 \
+  geodatasets \
+  pandas \
+  pyarrow \
+  scikit-learn \
+  seaborn \
+  tqdm \
+  xgboost \
+  jupyterlab -y
